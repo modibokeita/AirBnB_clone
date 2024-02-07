3515de0455f64b00b1e4dfcda961ab752a371c7a
Modibo keita <modibokeita@gmail.com>
2024-02-07 11:59:23 -0800

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,45 +1,39 @@
-#!/usr/bin/pyhon3
-"""
-BaseModel module
-"""
+#!/usr/bin/python3
+
 from uuid import uuid4
-import json
 from datetime import datetime
+"""
+Base Module
+"""
 
 
 class BaseModel:
     """
-    the Base module that the others
-    modules inherit from
+    initialization of instance attributes
     """
     def __init__(self, *args, **kwargs):
-
-        """
-        the initialization of
-        instances
-        """
         formated_time = "%Y-%m-%dT%H:%M:%S.%f"
-
         if kwargs:
             for key, value in kwargs.items():
-                if key == "__class__":
+                if key == "__build_class__":
                     continue
-                elif key == "created_at" or "updated_at":
+                elif key == "created_at" or key == "updated_at":
+
                     setattr(self, key, datetime.strptime(value, formated_time))
                 else:
                     setattr(self, key, value)
         else:
-            self.id = str(uuid())
-            self.created_date = datetime.utcnow()
-            self.update_at = datetime.utcnow()
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
-    def save():
-
+    def save(self):
         """
         updates the public instance attribute
         updated_at with the current datetime
         """
-        self.updated_at = datetime.utcnow()
+
+        self.updateed_at = datetime.utcnow()
 
     def to_dict(self):
         """
@@ -47,16 +41,18 @@ class BaseModel:
         keys/values of __dict__ of the instance
         """
 
-        dict_obj = self.__dict__copy()
-        dict_obj["__class__"] = self.__class__.__name__
-        dict_obj["created_at"] = self.created_at.isoformat()
-        dict_obj["update_at"] = self.updated_at.isoformat()
-        return dict_obj
+        obj_dict = self.__dict__.copy()
+
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
 
     def __str__(self):
-
         """
         should print: [<class name>] (<self.id>) <self.__dict__>
         """
+
         class_name = self.__class__.__name__
+
         return "[{}] {} {}".format(class_name, self.id, self.__dict__)
