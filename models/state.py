2db2edae5c87cfc1ba18343b66cef57b42153b09
Modibo keita <modibokeita@gmail.com>
2024-02-10 10:14:53 -0800

    state.py

---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+state module
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    that class inherit from
+    BaseModel class that allows
+    to access the attributes of
+    BaseModel class
+    """
+    name = ""
